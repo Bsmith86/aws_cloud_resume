6ac25e57d96d89ad1e36d947bb86b860f30e85b0
Bsmith86 <bsmith86.mlt@gmail.com>
2024-04-29 23:31:48 -0500

    workin out issues with api gateway CORS

--- brandons-aws/script.js
+++ brandons-aws/script.js
@@ -1,34 +1,34 @@
 // apiRequest.js
 
 // Replace this URL with your API Gateway endpoint
-const apiUrl = 'https://wj7z0n145l.execute-api.us-east-1.amazonaws.com';
+// const apiUrl = 'https://wj7z0n145l.execute-api.us-east-1.amazonaws.com';
 
-// Function to make a GET request to the API Gateway endpoint
-function makeApiRequest() {
-    fetch(apiUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log('API response:', data);
-            // You can perform further actions with the API response here
-        })
-        .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
-        });
-}
+// // Function to make a GET request to the API Gateway endpoint
+// function makeApiRequest() {
+//     fetch(apiUrl)
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error('Network response was not ok');
+//             }
+//             return response.json();
+//         })
+//         .then(data => {
+//             console.log('API response:', data);
+//             // You can perform further actions with the API response here
+//         })
+//         .catch(error => {
+//             console.error('There was a problem with the fetch operation:', error);
+//         });
+// }
 
 
 fetch('https://vp0vola44c.execute-api.us-east-2.amazonaws.com/Prod/get')
             .then(response => response.json())
             .then((data) => {
-                document.getElementById('count').innerText = data.count
+                document.getElementById('visitorCount').innerText = data.visitors
             })
-fetch('https://vp0vola44c.execute-api.us-east-2.amazonaws.com/Prod/put')
+fetch('https://vp0vola44c.execute-api.us-east-2.amazonaws.com/Prod/put', {mode: 'no-cors'})
             .then(response => response.json())
-            .then((data) => {
-                document.getElementById('count').innerText = data.count
-            })
+            // .then((data) => {
+            //     document.getElementById('count').innerText = data.count
+            // })
