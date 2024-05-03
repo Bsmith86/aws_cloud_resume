6a310ddc5ce5a0f66938f189e8bc23a89892110b
Brandon <bsmith86.mlt@gmail.com>
2024-05-02 19:44:31 -0500

    Put API Key added

--- brandons-aws/script.js
+++ brandons-aws/script.js
@@ -1,45 +1,18 @@
 // apiRequest.js
 
-// Replace this URL with your API Gateway endpoint
-// const apiUrl = 'https://wj7z0n145l.execute-api.us-east-1.amazonaws.com';
-
-// // Function to make a GET request to the API Gateway endpoint
-// function makeApiRequest() {
-//     fetch(apiUrl)
-//         .then(response => {
-//             if (!response.ok) {
-//                 throw new Error('Network response was not ok');
-//             }
-//             return response.json();
-//         })
-//         .then(data => {
-//             console.log('API response:', data);
-//             // You can perform further actions with the API response here
-//         })
-//         .catch(error => {
-//             console.error('There was a problem with the fetch operation:', error);
-//         });
-// }
-
-
 fetch('https://vp0vola44c.execute-api.us-east-2.amazonaws.com/Prod/get')
             .then(response => response.json())
             .then((data) => {
                 document.getElementById('visitorCount').innerText = data.visitors
             })
-// fetch('https://vp0vola44c.execute-api.us-east-2.amazonaws.com/Prod/put')
-//             .then(response => response.json())
-//             .then((data) => {
-//                 document.getElementById('count').innerText = data.count
-//             })
-// Function to make HTTP request to your API endpoint
+
 async function updateVisitorCount() {
     try {
       const response = await fetch('https://vp0vola44c.execute-api.us-east-2.amazonaws.com/Prod/put', {
         method: 'PUT', // Since your Lambda function expects a PUT request
-        mode: 'no-cors', // Set the mode to 'no-cors'
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
+          'x-api-key': 'L8dCAkQgOJ2tHGlHYWpxT1FAGmRu48vs6eL5Demq' // Include your API key here
           // Add any other headers if needed
         },
         body: JSON.stringify(data) // Replace 'data' with your request body
