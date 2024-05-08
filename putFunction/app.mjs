1fa058829178949c016e47e82ec063933dcc51ba
Brandon <bsmith86.mlt@gmail.com>
2024-05-07 21:26:46 -0500

    APIs fixed

--- putFunction/app.mjs
+++ putFunction/app.mjs
@@ -50,7 +50,7 @@ export const putFunction = async (event, context) => {
     const response = {
       statusCode: 200,
       headers: {
-        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Origin': 'https://brandonsaws.com',
         'Access-Control-Allow-Methods': '*',
         'Access-Control-Allow-Headers': 'Content-Type'
       },
